a35428ad94c5ad38b9a899bb4d4517b4be42a9dc
apushkin08 <apushkin08@gmail.com>
2019-01-17 17:27:41 +0300

    comment

--- js/cities.js
+++ js/cities.js
@@ -1,6 +1,6 @@
-$.getJSON("./json/russian-cities.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+// $.getJSON("./json/russian-cities.json", function(json) {
+//     console.log(json); // this will show the info it in firebug console
+// });
 
 // $(document).ready(() => {
 //     let list = 'russian-cities.json';
@@ -16,24 +16,24 @@ $.getJSON("./json/russian-cities.json", function(json) {
 // })
 
 
-// $(document).ready(() => {
-//     let list = 'russian-cities.json';
+$(document).ready(() => {
+    let list = 'russian-cities.json';
 
-//     fetch(list)
-//         .then(result => result.json())
-//         .then(data => {
-//             let $select = $('#city');
-//             let $cityData = $('<datalist/>', {
-//                 id: 'city-list'
-//             });
-//             $('.wrap').append($cityData);
-//             for (let city of data){
-//                 $cityData.append($(`<option value="${city.name}">${city.name}</option>`));
-//             }
-//             $select.keyup(() => {
-//                 if ($select.val().length > 2){
-//                     $select.attr('list', $cityData.attr('id'));
-//                 }
-//             })
-//         })
-// });
+    fetch(list)
+        .then(result => result.json())
+        .then(data => {
+            let $select = $('#city');
+            let $cityData = $('<datalist/>', {
+                id: 'city-list'
+            });
+            $('.wrap').append($cityData);
+            for (let city of data){
+                $cityData.append($(`<option value="${city.name}">${city.name}</option>`));
+            }
+            $select.keyup(() => {
+                if ($select.val().length > 2){
+                    $select.attr('list', $cityData.attr('id'));
+                }
+            })
+        })
+});
