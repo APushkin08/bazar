bbd875216fe5683153c3a98aedfd785dd6ea33f2
Maldades4689 <Maldades@mail.ru>
2019-01-18 17:57:43 +0300

    circle fix

--- js/circle.js
+++ js/circle.js
@@ -1,11 +1,19 @@
 $(document).ready(() => {
     $('.circles').on('click', '.circle:not(.active)',e =>{
+        console.log('MOZZZZZG');
         e.preventDefault();
         $(e.target).closest('.circles').find('.circle.active').removeClass('active');
         $(e.target).addClass('active')
             .closest('.deals-wrapp')
             .find('.circle-content')
             .hide();
-        $($(e.target).getElementsByClassName('test')).fadeIn(400);
+            $($(e.target).attr('class')).fadeIn(200);
     })
 })
+// $(document).ready(() => {
+//     $('.circles').on('click', '.circle:not(.active)', e =>{
+        
+//         e.preventDefault();
+//         $(e.target).closest('.circles').find('.circle.active').removeClass('active');
+//         // $(e.target).addClass('active')
+// })
