afda4a3e3ba6bd99e8e0940f5d83402ba79bcb30
apushkin08 <apushkin08@gmail.com>
2019-01-18 13:14:35 +0300

    fix search

--- js/cities.js
+++ js/cities.js
@@ -1,39 +1,63 @@
-// $.getJSON("./json/russian-cities.json", function(json) {
-//     console.log(json); // this will show the info it in firebug console
-// });
-
-$(document).ready(() => {
-    let list = 'https://raw.githubusercontent.com/APushkin08/bazar/master/json/russian-cities.json';
-    fetch(list)
-        .then(result => result.json())
-        .then(data => {
-            let $select = $('#city');
-            for (let city of data){
-                $select.append($(`<option value="${city.name}">${city.name}</option>`));
-            }
-        })
-
-})
-
-
 // $(document).ready(() => {
-//     let list = 'russian-cities.json';
-
+//     let list = 'https://raw.githubusercontent.com/APushkin08/bazar/master/json/russian-cities.json';
 //     fetch(list)
 //         .then(result => result.json())
 //         .then(data => {
 //             let $select = $('#city');
-//             let $cityData = $('<datalist/>', {
-//                 id: 'city-list'
-//             });
-//             $('.wrap').append($cityData);
 //             for (let city of data){
-//                 $cityData.append($(`<option value="${city.name}">${city.name}</option>`));
+//                 $select.append($(`<option value="${city.name}">${city.name}</option>`));
 //             }
+//         })
+
+// })
+
+
+// $(document).ready(() => {
+//     let list = 'https://raw.githubusercontent.com/APushkin08/bazar/master/json/russian-cities.json';
+//     fetch(list)
+//         .then(result => result.json())
+//         .then(data => {
+//             let $select = $('city');
 //             $select.keyup(() => {
-//                 if ($select.val().length > 2){
-//                     $select.attr('list', $cityData.attr('id'));
+//                 $('.list').remove();
+//                 if ($select.val().length > 2) {
+//                     let $block = $('<div/>', {
+//                         class: 'list'
+//                     });
+//                     let search = new RegExp($select.val(), 'i');
+//                     for (let city of data) {
+//                         if (search.test(city.name)) {
+//                             $block.append($(`<p class="city">${city.name}</p>`));
+
+//                         }
+//                     }
+//                     $block.appendTo($('.wrap'));
+//                     $block.on('click', 'p', e => {
+//                         $select.val(e.target.textContent)
+//                         $('.list').remove();
+//                     });
 //                 }
 //             })
 //         })
 // });
+$(document).ready(() => {
+    let list = 'https://raw.githubusercontent.com/APushkin08/bazar/master/json/russian-cities.json';
+
+    fetch(list)
+        .then(result => result.json())
+        .then(data => {
+            let $select = $('#city');
+            let $cityData = $('<datalist/>', {
+                id: 'city-list'
+            });
+            $('.wrap').append($cityData);
+            for (let city of data){
+                $cityData.append($(`<option value="${city.name}">${city.name}</option>`));
+            }
+            $select.keyup(() => {
+                if ($select.val().length > 2){
+                    $select.attr('list', $cityData.attr('id'));
+                }
+            })
+        })
+});
