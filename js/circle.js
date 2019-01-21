82a220559bfe8c687413d206bf72ca000a48b240
apushkin08 <apushkin08@gmail.com>
2019-01-21 17:56:27 +0300

    fix circle

--- js/circle.js
+++ js/circle.js
@@ -1,22 +1,29 @@
-$(document).ready(function slide() {
+$(document).ready(function() {
     $('.circles').on('click', '.circle:not(.active)', e => {
         // e.preventDefault();
-        $(e.target).closest('.circles').find('.circle.active').removeClass('active');
+        $(e.target).closest('.circles')
+            .find('.circle.active')
+            .removeClass('active');
+
         $(e.target).addClass('active')
             .closest('.hot-deals')
             .find('.circle-content')
             .hide();
+
         $($(e.target).attr('href')).fadeIn(200);
     });
-    var timerId = setInterval(function() {
+
+    var timerId1 = setInterval(function() {
         $('#circle2').trigger('click');
             console.log('MOZZZG');
       }, 5000);
-    var timerId = setInterval(function() {
+
+    var timerId2 = setInterval(function() {
         $('#circle3').trigger('click');
             console.log('MOZZZG');
       },10000);
-    var timerId = setInterval(function() {
+
+    var timerId3 = setInterval(function() {
         $('#circle1').trigger('click');
             console.log('MOZZZG');
       }, 15000);
