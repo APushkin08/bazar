6e57c9a5bf6ec00f9e3a2fecad89fdf5816b0fa8
Maldades4689 <Maldades@mail.ru>
2019-01-22 10:19:33 +0300

    circle fix

--- js/circle.js
+++ js/circle.js
@@ -15,21 +15,18 @@ $(document).ready(function() {
 
     var timerId1 = setInterval(function() {
         $('#circle2').trigger('click');
-            console.log('MOZZZG');
       }, 5000);
 
     var timerId2 = setInterval(function() {
         $('#circle3').trigger('click');
-            console.log('MOZZZG');
       },10000);
 
     var timerId3 = setInterval(function() {
         $('#circle1').trigger('click');
-            console.log('MOZZZG');
       }, 15000);
       
-      setTimeout(function() {
-        clearInterval(timerId);
-        console.log('MOZZZG');
-      }, 9000);
+    //   setTimeout(function() {
+    //     clearInterval(timerId);
+    //     console.log('MOZZZG');
+    //   }, 9000);
 });
